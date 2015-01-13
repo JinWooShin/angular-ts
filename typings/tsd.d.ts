2a3319bbaede9894c33e3072074408aa4399edcf
JinWooShin <jwshin@egi.utah.edu>
2015-01-12 21:01:09 -0700

    add files

---
+++ typings/tsd.d.ts
@@ -0,0 +1,5 @@
+/// <reference path="angularjs/angular.d.ts" />
+/// <reference path="jquery/jquery.d.ts" />
+/// <reference path="bootstrap/bootstrap.d.ts" />
+/// <reference path="d3/d3.d.ts" />
+/// <reference path="esri/esri.d.ts" />
